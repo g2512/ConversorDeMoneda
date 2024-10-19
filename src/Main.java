5dad55969a130bec7e48623a9f3c93ae251f8684
g2512 <ciancaguille@gmail.com>
2024-10-19 09:45:42 -0300

    otro commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner lectura=new Scanner(System.in);
+        System.out.println("Escriba el nombre de una moneda: ");
+        String busqueda= lectura.nextLine();
+        {
+            String direccion ="https://v6.exchangerate-api.com/v6/1f76ea4b8742f68115acb789/latest/"+busqueda;
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(direccion))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            String json=response.body();
+            System.out.println(json);
         }
+        System.out.println("Fin");
     }
 }
